6e0e4c2d433e213e7e3a1363be60b89bffdd3d8a
tnn1nja <thenonsenseninja@gmail.com>
2024-12-02 15:43:33 +0000

    Day 2 (Part 1 & 2)

---
+++ 2024/day2.py
@@ -0,0 +1,36 @@
+#Calculate if a report is valid (Part 1 & 2)
+def isValidReport(report, dampened=False, recursed=False):
+    growing = report[0] < report[1]
+    for i in range(len(report)-1):
+        diff = abs(report[i]-report[i+1])
+        if not ((report[i] < report[i+1]) == growing 
+                and diff < 4 and diff > 0):
+            if dampened and not recursed:
+                return isValidReport(stripIndex(report, i), True, True) or\
+                      isValidReport(stripIndex(report, i+1), True, True) or\
+                          (i == 1 and isValidReport(stripIndex(report, 0), True, True))
+            else:
+                return False
+    return True
+
+#Copy list without index
+def stripIndex(list, index):
+    output = list.copy()
+    output.pop(index)
+    return output
+
+#Read in file and run
+partOneAns = 0
+partTwoAns = 0
+with open("input.txt", "r") as file:
+    for line in file:
+        report = list(map(int, line.rstrip("\n").split(" ")))
+        if isValidReport(report):
+            partOneAns += 1
+            partTwoAns += 1
+        elif isValidReport(report, True):
+            partTwoAns += 1
+
+#Output
+print(f"Part One Answer: {partOneAns}")
+print(f"Part Two Answer: {partTwoAns}")
