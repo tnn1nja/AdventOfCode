db071d0a10c7c44f5e9dc85bb57e2c154fbad79f
tnn1nja <thenonsenseninja@gmail.com>
2024-12-03 15:57:59 +0000

    Day 3 (Fuck regex)

---
+++ 2024/day3.py
@@ -0,0 +1,17 @@
+import re
+
+#Calculate Multiples
+def findTotal(string):
+    total = 0
+    for pair in re.findall(r"mul\(.?.?.,.?.?.\)", string):
+        nums = pair.rstrip(")").lstrip("mul(").split(",")
+        total += int(nums[0])*int(nums[1])
+    return total
+
+#Read File
+with open("input.txt", "r") as f:
+    input = f.read().replace("\n", "")
+
+#Run and Output
+print(f"Part One Answer: {findTotal(input)}")
+print(f"Part Two Answer: {findTotal(re.sub(r"don't\(\)(.*?do\(\)|.*$)", "", input))}")
