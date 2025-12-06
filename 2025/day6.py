32cb605f25ed368df9a2b735954ad62c59f7100b
tnn1nja <thenonsenseninja@gmail.com>
2025-12-06 19:17:16 +0000

    slightly cleaned up?

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,4 +1,20 @@
-#Shared
+calcs = [[]]
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+    for collumn in range(len(lines[0])-1):
+        only_spaces = True
+        for row in range(len(lines)):
+            char = lines[row][collumn]
+            if char != " ": 
+                only_spaces = False
+            if len(calcs[-1]) > row:
+                calcs[-1][row] += char
+            else:
+                calcs[-1].append(char)
+        if only_spaces:
+            calcs[-1] = [x[:-1] for x in calcs[-1]]
+            calcs.append([])
+
 def doCalc(calc):
     operands = calc[:-1]
     if "+" in calc[-1]:
@@ -11,45 +27,18 @@ def doCalc(calc):
             total *= int(operand)
     return total
 
-#Input
-with open("input.txt", "r") as f:
-    lines = f.readlines()
-
-#Part 1
-sums = [[]]
-for collumn in range(len(lines[0])-1):
-    only_spaces = True
-    for row in range(len(lines)):
-        char = lines[row][collumn]
-        if char != " ": 
-            only_spaces = False
-        if len(sums[-1]) > row:
-            sums[-1][row] += char
-        else:
-            sums[-1].append(char)
-    if only_spaces:
-        sums[-1] = [x[:-1] for x in sums[-1]]
-        sums.append([])
-
 part_one = 0
-for s in sums:
-    part_one += doCalc(s)
-
-#Part 2
-for s in range(len(sums)):
-    new = []
-    for c in range(len(sums[s][0])):
-        num = ""
-        for n in sums[s][:-1]:
-            num += n[c]
-        new.append(num)
-    new.append(sums[s][-1])
-    sums[s] = new
-
 part_two = 0
-for s in sums:
-    part_two += doCalc(s)
+for calc in calcs:
+    part_one += doCalc(calc)
+    new = []
+    for i in range(len(calc[0])):
+        new_num = ""
+        for old_num in calc[:-1]:
+            new_num += old_num[i]
+        new.append(new_num)
+    new.append(calc[-1])
+    part_two += doCalc(new)
 
-#Output
 print(f"Part One Answer: {part_one}")
 print(f"Part Two Answer: {part_two}")
