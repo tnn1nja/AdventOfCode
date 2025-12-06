3d4c309de2876c48fbe40fb1ac7bf8279f27b402
tnn1nja <thenonsenseninja@gmail.com>
2025-12-06 18:17:48 +0000

    works finally

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,8 +1,35 @@
-import re
+with open("input.txt", "r") as f:
+    lines = f.readlines()
+
+part_one_sums = [[]]
+for collumn in range(len(lines[0])-1):
+    only_spaces = True
+    for row in range(len(lines)):
+        char = lines[row][collumn]
+        if char != " ": 
+            only_spaces = False
+        if len(part_one_sums[-1]) > row:
+            part_one_sums[-1][row] += char
+        else:
+            part_one_sums[-1].append(char)
+    if only_spaces:
+        part_one_sums[-1] = [x[:-1] for x in part_one_sums[-1]]
+        part_one_sums.append([])
+
+part_two_sums = []
+for old_sum in part_one_sums:
+    new_sum = []
+    for i in range(len(old_sum[0])):
+        num = ""
+        for n in old_sum[:-1]:
+            num += n[i]
+        new_sum.append(num)
+    new_sum.append(old_sum[-1])
+    part_two_sums.append(new_sum)
 
 def doCalc(calc):
     operands = calc[:-1]
-    if calc[-1] == "+":
+    if "+" in calc[-1]:
         total = 0
         for operand in operands:
             total += int(operand)
@@ -12,27 +39,13 @@ def doCalc(calc):
             total *= int(operand)
     return total
 
-with open("test.txt", "r") as f:
-    lines = f.readlines()
-
-part_one_sums = []
-s = [[] for x in lines]
-for collumn in range(len(lines[0])-1):
-    only_spaces = True
-    for row in range(len(lines)):
-        if lines[row][collumn] != " ":
-            only_spaces = False
-        s[row].append(lines[row][collumn])
-    if only_spaces:
-        part_one_sums.append(s)
-        s = [[] for x in lines]
-
-print(part_one_sums)
-
-"""
 part_one = 0
 for s in part_one_sums:
     part_one += doCalc(s)
 
+part_two = 0
+for s in part_two_sums:
+    part_two += doCalc(s)
+
 print(f"Part One Answer: {part_one}")
-"""
+print(f"Part Two Answer: {part_two}")
