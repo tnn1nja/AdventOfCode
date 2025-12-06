8ee6291ece9b275e23438288b7569c906c7dd772
tnn1nja <thenonsenseninja@gmail.com>
2025-12-06 17:21:30 +0000

    splits by spaces

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,16 +1,5 @@
 import re
 
-part_one_sums = []
-with open("test.txt", "r") as f:
-    for line_no, line in enumerate(f):
-        line = re.sub(r" +", " ", line.strip()).split(" ")
-        for i in range(len(line)):
-            if len(part_one_sums) > i:
-                part_one_sums[i].append(line[i])
-            else:
-                part_one_sums.append([line[i]])
-print(part_one_sums)
-
 def doCalc(calc):
     operands = calc[:-1]
     if calc[-1] == "+":
@@ -23,8 +12,27 @@ def doCalc(calc):
             total *= int(operand)
     return total
 
+with open("test.txt", "r") as f:
+    lines = f.readlines()
+
+part_one_sums = []
+s = [[] for x in lines]
+for collumn in range(len(lines[0])-1):
+    only_spaces = True
+    for row in range(len(lines)):
+        if lines[row][collumn] != " ":
+            only_spaces = False
+        s[row].append(lines[row][collumn])
+    if only_spaces:
+        part_one_sums.append(s)
+        s = [[] for x in lines]
+
+print(part_one_sums)
+
+"""
 part_one = 0
 for s in part_one_sums:
     part_one += doCalc(s)
 
 print(f"Part One Answer: {part_one}")
+"""
