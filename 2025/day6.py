a9dd6c14de0111aece77170ed283bb3df3e56544
tnn1nja <thenonsenseninja@gmail.com>
2025-12-06 12:59:52 +0000

    can't do it like this... need to keep track of alignment when reading them in

--- 2025/day6.py
+++ 2025/day6.py
@@ -1,27 +1,33 @@
 import re
 
 part_one_sums = []
-part_two_sums = []
 with open("test.txt", "r") as f:
     for line_no, line in enumerate(f):
         line = re.sub(r" +", " ", line.strip()).split(" ")
-        line = [int(x) if x.isdigit() else x for x in line]
         for i in range(len(line)):
             if len(part_one_sums) > i:
                 part_one_sums[i].append(line[i])
             else:
                 part_one_sums.append([line[i]])
+print(part_one_sums)
+
+part_two_sums = []
+for s in part_one_sums:
+    operator = s[-1]
+    new_sum = []
+    for operand in s[:-1]:
+        print(list(operand))    
 
 def doCalc(calc):
     operands = calc[:-1]
     if calc[-1] == "+":
         total = 0
         for operand in operands:
-            total += operand
+            total += int(operand)
     else:
         total = 1
         for operand in operands:
-            total *= operand
+            total *= int(operand)
     return total
 
 part_one = 0
